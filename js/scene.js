b06a1e423d2d8400822da69354195eb85afae8be
jofan <stefan.liden@gmail.com>
2013-05-11 17:29:45 +0200

    Some more scene stuff

--- js/scene.js
+++ js/scene.js
@@ -2,6 +2,12 @@ window.lj = lj || {};
 
 // ## Setting and updating the current scene of the game
 
+var spriteMap = {
+	'#': [18, 42], // wall
+	' ': [12, 42],
+	' ': [] // floor
+}
+
 lj.scene = (function() {
 
 	// Get the opposite door (for when entering and exiting)
@@ -18,7 +24,18 @@ lj.scene = (function() {
 
 	// Enter a room
 	function enter(door) {
+		var spriteImage = lj.getImage('dungeon-sprite.png'),
+			room = lj.realm.getCurrentRoom();
+
 		// Paint scene
+		for (var rowCtr=0;rowCtr<11;rowCtr++) {
+			for (var colCtr=0;colCtr<11;colCtr++){
+				var tileId = spriteMap[room[rowCtr][colCtr]];
+				var sourceX = Math.floor(tileId % 8) *32;
+				var sourceY = Math.floor(tileId / 8) *32;
+				lj.context.drawImage(spriteImage, sourceX, sourceY,32,32,colCtr*32,rowCtr*32,32,32);
+			}
+		}
 
 		// Animate hero entering from door
 		lj.hero.enter(door);
