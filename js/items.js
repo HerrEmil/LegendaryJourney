edc5522e27e8705980bfb9064e00e6f39ed6abdd
unknown <kma@kma-THINK.Agnitio.local>
2013-05-11 18:31:47 +0200

    properly namespaced items now

--- js/items.js
+++ js/items.js
@@ -69,22 +69,22 @@ window.lj = lj || {};
 		}
 		
 		if(!quality){
-			this.quality = window.items.qualities.get();
+			this.quality = lj.items.qualities.get();
 		} else {
 			this.quality = quality;
 		}
 
 		//Set the stats of the item based on type, prefix, affix and apply the quality multiplier
 		lj.stats.init(this);
-		this.strength = Math.round((this.prefix[0] + this.type[0] + this.suffix[0])*items.qualities[this.quality].mod);
-		this.agility = Math.round((this.prefix[1] + this.type[1] + this.suffix[1])*items.qualities[this.quality].mod);
-		this.luck = Math.round((this.prefix[2] + this.type[2] + this.suffix[2])*items.qualities[this.quality].mod);
-		this.hp = Math.round((this.prefix[3] + this.type[3] + this.suffix[3])*items.qualities[this.quality].mod);
-		this.hit = Math.round((this.prefix[4] + this.type[4] + this.suffix[4])*items.qualities[this.quality].mod);
-		this.crit = Math.round((this.prefix[5] + this.type[5] + this.suffix[5])*items.qualities[this.quality].mod);
-		this.armor = Math.round((this.prefix[6] + this.type[6] + this.suffix[6])*items.qualities[this.quality].mod);
-		this.defense = Math.round((this.prefix[7] + this.type[7] + this.suffix[7])*items.qualities[this.quality].mod);
-		this.magicfind = Math.round((this.prefix[8] + this.type[8] + this.suffix[8])*items.qualities[this.quality].mod);
+		this.strength = Math.round((this.prefix[0] + this.type[0] + this.suffix[0])*lj.items.qualities[this.quality].mod);
+		this.agility = Math.round((this.prefix[1] + this.type[1] + this.suffix[1])*lj.items.qualities[this.quality].mod);
+		this.luck = Math.round((this.prefix[2] + this.type[2] + this.suffix[2])*lj.items.qualities[this.quality].mod);
+		this.hp = Math.round((this.prefix[3] + this.type[3] + this.suffix[3])*lj.items.qualities[this.quality].mod);
+		this.hit = Math.round((this.prefix[4] + this.type[4] + this.suffix[4])*lj.items.qualities[this.quality].mod);
+		this.crit = Math.round((this.prefix[5] + this.type[5] + this.suffix[5])*lj.items.qualities[this.quality].mod);
+		this.armor = Math.round((this.prefix[6] + this.type[6] + this.suffix[6])*lj.items.qualities[this.quality].mod);
+		this.defense = Math.round((this.prefix[7] + this.type[7] + this.suffix[7])*lj.items.qualities[this.quality].mod);
+		this.magicfind = Math.round((this.prefix[8] + this.type[8] + this.suffix[8])*lj.items.qualities[this.quality].mod);
 
 
 		//Finally, give it a pretty name
@@ -128,15 +128,15 @@ window.lj = lj || {};
 			this.quality = null;
 		}
 		//Based on the slot picked, get a random type
-		this.itemType = items.types.get(ilvl,this.itemSlot);
+		this.itemType = lj.items.types.get(ilvl,this.itemSlot);
 		// console.log(this.itemType.name);
 
 		//Getting prefix
-		this.itemPrefix = lj.stats.prefixes.get(rng(0,window.stats.prefixes.length-1));
+		this.itemPrefix = lj.stats.prefixes.get(rng(0,lj.stats.prefixes.length-1));
 		// console.log(this.itemPrefix);
 
 		//Getting Suffix
-		this.itemSuffix = lj.stats.suffixes.get(rng(0,window.stats.suffixes.length-1));
+		this.itemSuffix = lj.stats.suffixes.get(rng(0,lj.stats.suffixes.length-1));
 		// console.log(this.itemSuffix);
 
 		//console.log(window.stats.prefixes[this.itemPrefix].name + ' ' +this.itemType.name+' '+window.stats.suffixes[this.itemSuffix].name);
@@ -150,64 +150,64 @@ window.lj = lj || {};
 //Base Items
 //------------
 	//Head
-	items.types.head.push(new iStats.fix([0,0,0,0,0,0,1,0,0],"Cap"));
-	items.types.head.push(new iStats.fix([0,1,0,1,0,1,0,0,0],"Hood"));
-	items.types.head.push(new iStats.fix([0,0,0,0,0,0,2,2,0],"Helmet"));
-	items.types.head.push(new iStats.fix([1,1,0,1,0,0,0,0,0],"Bandana"));
-	items.types.head.push(new iStats.fix([0,0,2,0,0,0,0,0,2],"Top Hat"));
+	lj.items.types.head.push(new iStats.fix([0,0,0,0,0,0,1,0,0],"Cap"));
+	lj.items.types.head.push(new iStats.fix([0,1,0,1,0,1,0,0,0],"Hood"));
+	lj.items.types.head.push(new iStats.fix([0,0,0,0,0,0,2,2,0],"Helmet"));
+	lj.items.types.head.push(new iStats.fix([1,1,0,1,0,0,0,0,0],"Bandana"));
+	lj.items.types.head.push(new iStats.fix([0,0,2,0,0,0,0,0,2],"Top Hat"));
 	//Shoulder
-	items.types.shoulder.push(new iStats.fix([0,0,0,0,0,0,2,2,0],"Pauldrons"));
-	items.types.shoulder.push(new iStats.fix([0,0,0,0,2,2,0,0,0],"Shoulder Spikes"));
-	items.types.shoulder.push(new iStats.fix([2,0,0,0,0,0,1,1,0],"Pads"));
-	items.types.shoulder.push(new iStats.fix([0,0,0,0,0,0,0,0,1],"Parrot"));
+	lj.items.types.shoulder.push(new iStats.fix([0,0,0,0,0,0,2,2,0],"Pauldrons"));
+	lj.items.types.shoulder.push(new iStats.fix([0,0,0,0,2,2,0,0,0],"Shoulder Spikes"));
+	lj.items.types.shoulder.push(new iStats.fix([2,0,0,0,0,0,1,1,0],"Pads"));
+	lj.items.types.shoulder.push(new iStats.fix([0,0,0,0,0,0,0,0,1],"Mantle"));
 	//Chest
-	items.types.chest.push(new iStats.fix([0,1,0,3,0,0,0,0,0],"Robe"));
-	items.types.chest.push(new iStats.fix([0,0,0,0,0,0,2,2,0],"Breastplate"));
-	items.types.chest.push(new iStats.fix([1,0,0,1,0,0,1,1,0],"Vest"));
-	items.types.chest.push(new iStats.fix([0,0,0,0,0,0,0,4,0],"Chainmail"));
+	lj.items.types.chest.push(new iStats.fix([0,1,0,3,0,0,0,0,0],"Robe"));
+	lj.items.types.chest.push(new iStats.fix([0,0,0,0,0,0,2,2,0],"Breastplate"));
+	lj.items.types.chest.push(new iStats.fix([1,0,0,1,0,0,1,1,0],"Vest"));
+	lj.items.types.chest.push(new iStats.fix([0,0,0,0,0,0,0,4,0],"Chainmail"));
 	//Cloak
-	items.types.cloak.push(new iStats.fix([0,0,3,0,0,0,0,0,0],"Cape"));
-	items.types.cloak.push(new iStats.fix([0,0,0,0,0,0,0,0,2],"Backpack"));
-	items.types.cloak.push(new iStats.fix([0,2,0,0,0,2,0,0,0],"Scarf"));
+	lj.items.types.cloak.push(new iStats.fix([0,0,3,0,0,0,0,0,0],"Cape"));
+	lj.items.types.cloak.push(new iStats.fix([0,0,0,0,0,0,0,0,2],"Backpack"));
+	lj.items.types.cloak.push(new iStats.fix([0,2,0,0,0,2,0,0,0],"Scarf"));
 	//Gloves
-	items.types.gloves.push(new iStats.fix([0,0,0,0,0,0,2,2,0],"Gauntlets"));
-	items.types.gloves.push(new iStats.fix([0,2,0,0,0,0,1,1,0],"Gloves"));
-	items.types.gloves.push(new iStats.fix([3,0,0,0,0,2,0,0,0],"Brass Knuckles"));
+	lj.items.types.gloves.push(new iStats.fix([0,0,0,0,0,0,2,2,0],"Gauntlets"));
+	lj.items.types.gloves.push(new iStats.fix([0,2,0,0,0,0,1,1,0],"Gloves"));
+	lj.items.types.gloves.push(new iStats.fix([3,0,0,0,0,2,0,0,0],"Brass Knuckles"));
 	//Pants
-	items.types.pants.push(new iStats.fix([1,1,1,1,0,0,0,0,0],"Slacks"));
-	items.types.pants.push(new iStats.fix([0,2,0,0,0,1,0,0,1],"Dress Pants"));
-	items.types.pants.push(new iStats.fix([1,0,0,0,2,0,0,0,0],"Kilt"));
-	items.types.pants.push(new iStats.fix([0,0,0,0,0,0,2,3,0],"Legplates"));
+	lj.items.types.pants.push(new iStats.fix([1,1,1,1,0,0,0,0,0],"Slacks"));
+	lj.items.types.pants.push(new iStats.fix([0,2,0,0,0,1,0,0,1],"Dress Pants"));
+	lj.items.types.pants.push(new iStats.fix([1,0,0,0,2,0,0,0,0],"Kilt"));
+	lj.items.types.pants.push(new iStats.fix([0,0,0,0,0,0,2,3,0],"Legplates"));
 	//Boots
-	items.types.boots.push(new iStats.fix([0,1,0,0,2,1,0,0,0],"Jaki-tabis"));
-	items.types.boots.push(new iStats.fix([0,2,0,0,0,0,0,1,0],"Sneakers"));
-	items.types.boots.push(new iStats.fix([0,0,0,1,0,0,2,2,0],"Greaves"));
-	items.types.boots.push(new iStats.fix([0,1,0,1,0,0,2,3,1],"Dress shoes"));
+	lj.items.types.boots.push(new iStats.fix([0,1,0,0,2,1,0,0,0],"Jaki-tabis"));
+	lj.items.types.boots.push(new iStats.fix([0,2,0,0,0,0,0,1,0],"Sneakers"));
+	lj.items.types.boots.push(new iStats.fix([0,0,0,1,0,0,2,2,0],"Greaves"));
+	lj.items.types.boots.push(new iStats.fix([0,1,0,1,0,0,2,3,1],"Dress Shoes"));
 	//Ring
-	items.types.ring.push(new iStats.fix([0,0,0,3,0,0,1,0,0],"Bronze Ring"));
-	items.types.ring.push(new iStats.fix([1,2,0,0,0,0,0,1,0],"Silver Ring"));
-	items.types.ring.push(new iStats.fix([0,0,1,0,0,1,0,0,1],"Gold Ring"));
-	items.types.ring.push(new iStats.fix([0,0,0,1,0,0,2,3,0],"Stone Band"));
+	lj.items.types.ring.push(new iStats.fix([0,0,0,3,0,0,1,0,0],"Bronze Ring"));
+	lj.items.types.ring.push(new iStats.fix([1,2,0,0,0,0,0,1,0],"Silver Ring"));
+	lj.items.types.ring.push(new iStats.fix([0,0,1,0,0,1,0,0,1],"Gold Ring"));
+	lj.items.types.ring.push(new iStats.fix([0,0,0,1,0,0,2,3,0],"Stone Band"));
 	//Trinket
 
 //------------
 //Weapons
 //------------
 	//Swords
-	items.types.weapon.push(new iStats.fix([3,4,0,0,2,0,0,0,0],"Dagger"));
-	items.types.weapon.push(new iStats.fix([6,1,0,0,2,0,0,0,0],"Claymore"));
-	items.types.weapon.push(new iStats.fix([0,6,0,0,2,0,0,0,0],"Katana"));
-	items.types.weapon.push(new iStats.fix([3,3,0,0,2,1,0,0,0],"Schimitar"));
+	lj.items.types.weapon.push(new iStats.fix([3,4,0,0,2,0,0,0,0],"Dagger"));
+	lj.items.types.weapon.push(new iStats.fix([6,1,0,0,2,0,0,0,0],"Claymore"));
+	lj.items.types.weapon.push(new iStats.fix([0,6,0,0,2,0,0,0,0],"Katana"));
+	lj.items.types.weapon.push(new iStats.fix([3,3,0,0,2,1,0,0,0],"Schimitar"));
 	//Hammers
-	items.types.weapon.push(new iStats.fix([9,-1,0,0,0,0,0,-1,0],"Stone Maul"));
-	items.types.weapon.push(new iStats.fix([7,0,0,1,0,0,0,0,0],"Mace"));
-	items.types.weapon.push(new iStats.fix([3,2,0,0,0,0,0,0,2],"Cane"));
+	lj.items.types.weapon.push(new iStats.fix([9,-1,0,0,0,0,0,-1,0],"Stone Maul"));
+	lj.items.types.weapon.push(new iStats.fix([7,0,0,1,0,0,0,0,0],"Mace"));
+	lj.items.types.weapon.push(new iStats.fix([3,2,0,0,0,0,0,0,2],"Cane"));
 	//Axes
-	items.types.weapon.push(new iStats.fix([2,5,0,0,0,2,0,0,0],"Hatchet"));
-	items.types.weapon.push(new iStats.fix([3,3,0,0,1,2,0,0,0],"Cleaver"));
-	items.types.weapon.push(new iStats.fix([5,2,0,0,0,2,0,0,0],"Double Axe"));
+	lj.items.types.weapon.push(new iStats.fix([2,5,0,0,0,2,0,0,0],"Hatchet"));
+	lj.items.types.weapon.push(new iStats.fix([3,3,0,0,1,2,0,0,0],"Cleaver"));
+	lj.items.types.weapon.push(new iStats.fix([5,2,0,0,0,2,0,0,0],"Double Axe"));
 
-	items.types.get = function(level,slot,type){
+	lj.items.types.get = function(level,slot,type){
 		var ilvl,
 			gear,
 			rng = lj.util.randomInterval,
@@ -257,10 +257,10 @@ window.lj = lj || {};
 		return statArray;
 	};
 }());
-var tonsofitems = function(num){
+lj.items.lots = function(num){
 	var num, i,obj;
 	for(i=0;i<num;i++){
-		obj = items.makeItem();
+		obj = lj.items.makeItem();
 		console.log(obj.name);
 	}
 }
