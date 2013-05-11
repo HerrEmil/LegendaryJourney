daf51e4f0db8f97530f36b719abb02c627df0017
jofan <stefan.liden@gmail.com>
2013-05-11 04:22:49 +0200

    Added loading message and some styles to splash

--- js/game-startup.js
+++ js/game-startup.js
@@ -1,34 +1,55 @@
 
 (function() {
 
-	window.lj = new Game();
+	var intro = document.getElementById('gameIntro'),
+		start = intro.querySelector('.btn-start');
 
-	var images = [
-		'dungeon-sprite.png',
-		'hero-walk.png',
-		'armor-sprite.png',
-		'camp-fire.png',
-		'crystal-blue.png',
-		'crystal-green.png',
-		'crystal-grey.png',
-		'crystal-orange.png',
-		'crystal-pink.png',
-		'crystal-yellow.png',
-		'orc-sprite.png'
-	];
+	start.addEventListener('click', startGame);
 
-	images.forEach(function(image) {
-		lj.queueImage(image);
-	});
+	function startGame() {
 
-	var interval = setInterval(function() {
-		var completed = lj.loadImages();
-		if (completed === 100) {
-			clearInterval(interval);
-			console.log('Complete! All images loaded.');
-			document.body.appendChild(lj.getImage('hero-walk.png'));
-		}
-		console.log(completed);
-	},16);
+		var progress = intro.querySelector('#progress'),
+			progressBar = progress.querySelector('.progress-bar'),
+			images,
+			interval;
+
+		window.lj = new Game();
+
+		start.style.display = 'none';
+		progress.style.display = 'block';
+
+		images = [
+			'dungeon-sprite.png',
+			'hero-walk.png',
+			'armor-sprite.png',
+			'camp-fire.png',
+			'crystal-blue.png',
+			'crystal-green.png',
+			'crystal-grey.png',
+			'crystal-orange.png',
+			'crystal-pink.png',
+			'crystal-yellow.png',
+			'orc-sprite.png'
+		];
+
+		images.forEach(function(image) {
+			lj.queueImage(image);
+		});
+
+		interval = setInterval(function() {
+			var completed = lj.loadImages();
+			progressBar.style.width = completed * 2 + 'px';
+			if (completed === 100) {
+				clearInterval(interval);
+				setTimeout(function() {
+					intro.setAttribute('class', 'faded');
+					setTimeout(function() {
+						intro.style.display = 'none';
+					},500);
+				},1000);
+			}
+			console.log(completed);
+		},16);
+	}
 
 }());
