97cefa6c8cd0ddfca62aec015b5a5f7f8b2ec923
jofan <stefan.liden@gmail.com>
2013-05-11 20:22:39 +0200

    Our hero is walking

--- js/hero.js
+++ js/hero.js
@@ -4,7 +4,8 @@ lj.hero = (function() {
 	'use strict'
 
 	var currentDir = null,
-		currentTile = [0,0]; // row, col
+		room = null,
+		currentTile = [5,9]; // col, row
 
 	var stepModifiers = {
 		left: [0, -1],
@@ -13,16 +14,20 @@ lj.hero = (function() {
 		down: [1, 0]
 	}
 
+	setupListeners();
+
 	// The hero enters the room...
 	function enter(door) {
 		// Get the starting tile from realm.js (get door tile)
-		var tile = [0, 0];
-		place(tile);
-		setupListeners();
+		var currentRoom = lj.realm.getCurrentRoom();
+		room = lj.realm.getRoom(currentRoom);
+		console.log(currentRoom);
+		place(currentTile);
+		console.log(currentTile);
 	}
 
 	// The hero exits the room...
-	function exit() {
+	function exit(door) {
 
 	}
 
@@ -42,26 +47,65 @@ lj.hero = (function() {
 
 	// PRIVATE: place the hero in the room
 	function place(tile) {
+		var spriteImage = lj.getImage('dungeon-sprite.png');
 		// Figure out in what direction to face based on tile #
 		currentTile = tile;
+		lj.context.drawImage(
+			spriteImage, 4*32, 2*32,32,32,tile[0]*32,tile[1]*32,32,32);
+	}
+
+	function move(tile) {
+		var type = room[tile[0]][tile[1]];
+		var spriteImage = lj.getImage('dungeon-sprite.png');
+		// Find out what type of tile we're moving to
+		switch (type) {
+			case '#': return; break;
+			case ' ':
+				currentTile = tile;
+				console.log('Hero moved to:', currentTile, currentDir);
+				lj.context.drawImage(
+					spriteImage, 4*32, 2*32,32,32,tile[0]*32,tile[1]*32,32,32);
+				break;
+			case 'D':
+				currentTile[1] = 0;
+				lj.scene.exit(type);
+				break;
+			case 'U':
+				currentTile[1] = 10;
+				lj.scene.exit(type);
+				break;
+			case 'L':
+				currentTile[0] = 10;
+				lj.scene.exit(type);
+				break;
+			case 'R':
+				currentTile[0] = 0;
+				lj.scene.exit(type);
+				break;
+			default:
+				interact(type);
+		}
+	}
+
+	function interact(type) {
+		console.log('Our here will interact with', type);
 	}
 
 	// Makes the hero step in a direction
 	// Directions: left, right, up, down
 	function step(dir) {
-		var row, col;
+		var row, col, nextTile;
 		// Is the hero turned in the right direction?
 		if (dir !== currentDir) turn(dir);
 
+		// Find new coordinates. Need to relate to the grid
+		row = currentTile[1] + stepModifiers[dir][0];
+		col = currentTile[0] + stepModifiers[dir][1];
+
+		nextTile = [col, row];
+
 		// Any restrictions, then don't move?
-		if (isRestricted(dir)) return;
-
-		// Make the step. Need to relate to the grid
-		row = currentTile[0] + stepModifiers[dir][0];
-		col = currentTile[1] + stepModifiers[dir][1];
-		currentTile = [row, col];
-
-		console.log('Hero moved to:', currentTile, currentDir);
+		move(nextTile);
 	}
 
 	// Turn the hero into the direction
--- js/scene.js
+++ js/scene.js
@@ -2,21 +2,47 @@ window.lj = lj || {};
 
 // ## Setting and updating the current scene of the game
 
+var floor = [14, 49]
+
 var spriteMap = {
-	'#': [18, 42], // wall
-	' ': [12, 42],
-	' ': [] // floor
+	'#': [18, 44], // wall
+	' ': floor, // floor
+	'L': floor, // Door leading Left
+	'U': floor, // Door leading Up
+	'R': floor, // Door leading Right
+	'D': floor, // Door leading Down
+
+	'a': floor, // Monster type a
+	'b': floor, // Monster type b
+	'c': floor, // Monster type c
+	'd': floor, // ...
+	'z': floor, // Monster type z
+
+	'1': floor, // Chest type 1
+	'2': floor, // Chest type 2
+	'3': floor, // ...
+
+	'9': floor // Chest type 9 
+}
+
+var roomModifier = {
+	'D': 3,
+	'U': -3,
+	'L': -1,
+	'R': 1
 }
 
 lj.scene = (function() {
 
+	var currentRoom = null;
+
 	// Get the opposite door (for when entering and exiting)
 	function oppositeDoor(door) {
 		var opposites = {
-			LEFT: 'RIGHT',
-			RIGHT: 'LEFT',
-			UP: 'DOWN',
-			DOWN: 'UP'
+			L: 'R',
+			R: 'L',
+			U: 'D',
+			D: 'U'
 		}
 
 		return opposites[door];
@@ -25,14 +51,18 @@ lj.scene = (function() {
 	// Enter a room
 	function enter(door) {
 		var spriteImage = lj.getImage('dungeon-sprite.png'),
-			room = lj.realm.getCurrentRoom();
+			room = null;
+
+		if (!currentRoom) currentRoom = lj.realm.getCurrentRoom();
+
+		room = lj.realm.getRoom(currentRoom);
 
 		// Paint scene
-		for (var rowCtr=0;rowCtr<11;rowCtr++) {
-			for (var colCtr=0;colCtr<11;colCtr++){
-				var tileId = spriteMap[room[rowCtr][colCtr]];
-				var sourceX = Math.floor(tileId % 8) *32;
-				var sourceY = Math.floor(tileId / 8) *32;
+		for (var rowCtr = 0; rowCtr < 11; rowCtr++) {
+			for (var colCtr = 0; colCtr < 11; colCtr++) {
+				var tileId = spriteMap[room[colCtr][rowCtr]],
+					sourceX = Math.floor(tileId[1]) * 32,
+					sourceY = Math.floor(tileId[0]) * 32;
 				lj.context.drawImage(spriteImage, sourceX, sourceY,32,32,colCtr*32,rowCtr*32,32,32);
 			}
 		}
@@ -42,8 +72,12 @@ lj.scene = (function() {
 	}
 
 	// Exit the current room through a door
-	// Door can be "UP", "DOWN", "LEFT", "RIGHT"
+	// Door can be "U", "D", "L", "R"
 	function exit(door) {
+
+		currentRoom = currentRoom + roomModifier[door];
+		console.log(currentRoom);
+		lj.realm.makeRoom(currentRoom);
 
 		lj.hero.exit();
 
@@ -51,6 +85,11 @@ lj.scene = (function() {
 
 		// When exiting hero is also entering the next room (or realm)
 		enter(oppositeDoor(door));
+	}
+
+	return {
+		enter: enter,
+		exit: exit
 	}
 
 }());
