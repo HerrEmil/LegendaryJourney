e82f2eec357d5ec5ae39af47a8381083158254c7
Bird <kriden@gmail.com>
2013-05-12 15:43:10 +0200

    updated enemy handling and realm scaling

--- js/enemy.js
+++ js/enemy.js
@@ -2,31 +2,32 @@ window.lj = lj || {};
 lj.enemy = {};
 
 lj.enemy.base = function(type,mod){
-	this.strength = Math.round(type.stats[0]*mod.stats[0]);
-	this.agility = Math.round(type.stats[1]*mod.stats[1]);
-	this.hp = Math.round(type.stats[2]*mod.stats[2]);
-	this.hit = Math.round(type.stats[3]*mod.stats[3]);
-	this.crit = Math.round(type.stats[4]*mod.stats[4]);
-	this.armor = Math.round(type.stats[5]*mod.stats[5]);
-	this.defense = Math.round(type.stats[6]*mod.stats[6]);
+	var realmMod = (lj.realm.getSize()+9)/10;
+	this.strength = Math.round(type.stats[0]*mod.stats[0]*realmMod);
+	this.agility = Math.round(type.stats[1]*mod.stats[1]*realmMod);
+	this.hp = Math.round(type.stats[2]*mod.stats[2]*realmMod);
+	this.hit = Math.round(type.stats[3]*mod.stats[3]*realmMod);
+	this.crit = Math.round(type.stats[4]*mod.stats[4]*realmMod);
+	this.armor = Math.round(type.stats[5]*mod.stats[5]*realmMod);
+	this.defense = Math.round(type.stats[6]*mod.stats[6]*realmMod);
 	this.name = (mod.name+' '+type.name).trim();
 }
-lj.enemy.get = function(type,boss){
+lj.enemy.get = function(type,grade){
 	var rng = lj.util.randomInterval,
-		mon = lj.enemy.types[type][rng(0,lj.enemy.types[type].length-1)],
+		mon = lj.enemy.types[type][grade],
 		mod = lj.enemy.mods[rng(0,lj.enemy.mods.length-1)];
-	if(boss){
-		return new lj.enemy.base(mon,{stats:[3,3,3,3,3,1,1],name:"Big Bad Boss"})
-	};
+	if(rng(1,5)<5){
+		return new lj.enemy.base(mon,{stats:[1,1,1,1,1,1,1],name:""});
+	}
 	// console.log(mon);
 	// console.log(mod);
 	return new lj.enemy.base(mon,mod);
 };
 lj.enemy.types = {
-	brown : [],
-	blue : [],
-	green : [],
-	red : []
+	brown : {},
+	blue : {},
+	green : {},
+	red : {}
 };
 lj.enemy.mods = [];
 
@@ -42,10 +43,11 @@ lj.enemy.mods = [];
 		mon = lj.enemy.monster,
 		mod = lj.enemy.mods;
 	//Brown
-	col["brown"].push(new mon([2,2,25,1,1,1,2],"Wolf"));
-	col["brown"].push(new mon([3,1,20,0,1,2,0],"Goblin"));
-	col["brown"].push(new mon([1,4,35,1,2,1,1],"Troll"));
-	col["brown"].push(new mon([3,1,40,1,1,5,0],"Earth Elemental"));
+	col["brown"].a = new mon([2,2,25,1,1,1,2],"Wolf");
+	col["brown"].b = new mon([3,1,20,0,1,2,0],"Gnoll");
+	col["brown"].c = new mon([1,4,35,1,2,1,1],"Troll");
+	col["brown"].d = new mon([3,2,40,1,1,5,0],"Undead Mage");
+	col["brown"].B = new mon([7,7,80,1,1,5,0],"Evil Blue Mage");
 	//TODO: Blue, green and red
 
 	//Set up mods
