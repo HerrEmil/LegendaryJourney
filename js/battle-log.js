ea2ed56493a21217b44b83bae4ce3fcf564022a9
Emil Andersson <ean@anthillagency.com>
2013-05-12 12:47:12 +0200

    Functioning battle log

--- js/battle-log.js
+++ js/battle-log.js
@@ -3,19 +3,62 @@ window.lj = lj || {};
 lj.battleLog = (function () {
 	"use strict";
 
-	var div = document.getElementById('battleLog');
+	var battleLog = document.getElementById('battleLog');
 
-	function scrollDown() {
-		div.scrollTop = div.scrollHeight - div.clientHeight;
+	function printMessage(text) {
+		// Create text node and break tag
+		var newMessage = document.createTextNode(text),
+			br = document.createElement("br");
+		// Add new text and tag to HTML
+		battleLog.appendChild(newMessage);
+		battleLog.appendChild(br);
+		// Scroll to bottom of scrollbox
+		battleLog.scrollTop = battleLog.scrollHeight - battleLog.clientHeight;
 	}
 
-	function addText(text) {
-		// Insert text in HTML
-		scrollDown();
+	function aqcuire(item) {
+		var message = "You found " + item + "!";
+		printMessage(message);
+	}
+
+	function autoEquip(item) {
+		var message1 = "It's better than what you have!",
+			message2 = "You equipped " + item + "!";
+		printMessage(message1);
+		printMessage(message2);
+	}
+
+	function monsterKilled(monster) {
+		var message = "Slayed " + monster + "!";
+		printMessage(message);
+	}
+
+	function heroKilled(monster) {
+		var message = "You were killed by " + monster + ":(";
+		printMessage(message);
+	}
+
+	function levelComplete(level) {
+		var message = "You finished level " + level + "! :D";
+		printMessage(message);
+	}
+
+	function gameComplete() {
+		printMessage("You completed the game!");
+		printMessage("You are AWESOME! Congratulations!");
+	}
+
+	function customMessage(message) {
+		printMessage(message);
 	}
 
 	return {
-		addText : addText,
-		scrollDown : scrollDown
+		aqcuire : aqcuire,
+		autoEquip : autoEquip,
+		monsterKilled : monsterKilled,
+		heroKilled : heroKilled,
+		levelComplete : levelComplete,
+		gameComplete : gameComplete,
+		customMessage : customMessage
 	};
 }());
